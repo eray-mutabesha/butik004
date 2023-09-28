0c7f401ceab4d8cf703a6e510cf3e7604cc74378
Francis Hakim BC <francishakimbc@Franciss-MacBook-Air.local>
2023-09-28 23:03:28 +0200

    changes some design

---
+++ index.js
@@ -0,0 +1,22 @@
+// const leftbtn=document.querySelector(".iconright");
+// let i=0;
+// let a=0;
+// let images=[];
+// images=["images/pillnoire.jpeg","images/az.jpg","images/j1white.jpeg"];
+
+
+// function imagesChange(){
+//     document.slide.src=images[i];
+//     if(i < images.length -1){
+//         i++;
+//     }
+//     else{
+//         i=0;
+//     }
+    
+// }
+// window.onload=imagesChange();
+
+
+
+
