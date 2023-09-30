1a934cc80817570966a84c3feefa589450cf4ae4
Francis Hakim BC <francishakimbc@Franciss-MacBook-Air.local>
2023-09-30 16:49:22 +0200

    add menu

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-// const leftbtn=document.querySelector(".iconright");
-// let i=0;
-// let a=0;
-// let images=[];
-// images=["images/pillnoire.jpeg","images/az.jpg","images/j1white.jpeg"];
 
+const shoesdiv=document.getElementById("image_shoes");
+let images=[];
+images=["images/jordan4r.webp","images/jordna4.webp"];
+let i=0;
+function shoes(){
+document.shoes.src=images[i];
+if(i<images.length -1){
+    i++;
+}
+else{
+    i=0;
+}
+setTimeout("shoes()",2000);
+}
+window.onload=shoes();
 
-// function imagesChange(){
-//     document.slide.src=images[i];
-//     if(i < images.length -1){
-//         i++;
-//     }
-//     else{
-//         i=0;
-//     }
-    
-// }
-// window.onload=imagesChange();
-
-
-
+const menu=document.querySelector(".menu");
+const icon=document.getElementById("icon");
+icon.addEventListener("click",()=>{
+   menu.classList.toggle("menu_deroulant");
+   
+})
 
